f1c06e9bdaf89c17e7bdfd3465a765e6328f1713
Dogan Yazar <dogan.yazar@expressen.se>
2017-09-21 14:06:38 +0200

    Add test for case insensitivity

--- test/spec.js
+++ test/spec.js
@@ -3,7 +3,6 @@
 const test = require('tape')
 const stemmer = require('../')
 
-
 test('Get regions', t => {
   t.deepEqual(stemmer.getRegions('jakt'), {r1: 't', rest: 'jak'})
   t.deepEqual(stemmer.getRegions('klorna'), {r1: 'na', rest: 'klor'})
@@ -16,6 +15,12 @@ test('Stem sample', t => {
   const sample = require('./data/sample.js')
   const res = sample.words.map(w => stemmer.stem(w))
   t.deepEqual(res, sample.stemmedWords)
+  t.end()
+})
+
+test('Should convert to lowercase', t => {
+  t.equal(stemmer.stem("bJöRks"), "björk")
+  t.equal(stemmer.stem("BJörkS"), "björk")
   t.end()
 })
 
